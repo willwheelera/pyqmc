c936e3cb7cd8b35a51bb835728dd2e7a087a7b62
Will Wheeler <willwheelera@gmail.com>
2021-06-30 23:44:02 -0500

    change coord to kptcoord

--- pyqmc/coord.py
+++ pyqmc/coord.py
@@ -4,103 +4,7 @@ import pyqmc.pbc as pbc
 import copy
 
 
-class OpenElectron:
-    def __init__(self, epos, dist):
-        self.configs = epos
-        self.dist = dist
-
-
-class OpenConfigs:
-    def __init__(self, configs):
-        self.configs = configs
-        self.dist = distance.RawDistance()
-
-    def electron(self, e):
-        return OpenElectron(self.configs[:, e], self.dist)
-
-    def mask(self, mask):
-        return OpenConfigs(self.configs[mask])
-
-    def make_irreducible(self, e, vec, mask=True):
-        """
-        Input:
-          e: unused electron index
-          vec: a (nconfig, 3) vector
-        Output: OpenConfigs object with just one electron
-        """
-        return OpenElectron(vec, self.dist)
-
-    def move(self, e, new, accept):
-        """
-        Change coordinates of one electron
-        Args:
-          e: int, electron index
-          new: OpenConfigs with (nconfig, 3) new coordinates
-          accept: (nconfig,) boolean for which configs to update
-        """
-        self.configs[accept, e, :] = new.configs[accept, :]
-
-    def move_all(self, new, accept):
-        """
-        Change coordinates of all electrons
-        Args:
-          new: OpenConfigs with configs.shape new coordinates
-          accept: (nconfig,) boolean for which configs to update
-        """
-        self.configs[accept] = new.configs[accept]
-
-    def resample(self, newinds):
-        """
-        Resample configs by new indices (e.g. for DMC branching)
-        Args:
-          newinds: (nconfigs,) array of indices
-        """
-        self.configs = self.configs[newinds]
-
-    def split(self, npartitions):
-        """
-        Split configs into npartitions new configs objects for parallelization
-        Args:
-          npartitions: int, number of partitions to divide configs into
-        Returns:
-          configslist: list of new configs objects
-        """
-        return [OpenConfigs(c) for c in np.array_split(self.configs, npartitions)]
-
-    def join(self, configslist, axis=0):
-        """
-        Merge configs into this object to collect from parallelization
-        Args:
-          configslist: list of OpenConfigs objects
-        """
-        self.configs = np.concatenate([c.configs for c in configslist], axis=axis)
-
-    def copy(self):
-        return copy.deepcopy(self)
-
-    def reshape(self, shape):
-        self.configs = self.configs.reshape(shape)
-
-    def initialize_hdf(self, hdf):
-        hdf.create_dataset(
-            "configs",
-            self.configs.shape,
-            chunks=True,
-            maxshape=(None, *self.configs.shape[1:]),
-        )
-
-    def to_hdf(self, hdf):
-        hdf["configs"].resize(self.configs.shape)
-        hdf["configs"][...] = self.configs
-
-    def load_hdf(self, hdf):
-        """Note that the number of configurations will change to reflect the number in the hdf file."""
-        # The ... seems to be necessary to avoid changing the dtype and screwing up
-        # pyscf's calls.
-        self.configs[...] = np.array(hdf["configs"])
-
-
-class PeriodicElectron:
+class KptElectron:
     """
     Represents the coordinates of a test electron position, for many walkers and
     potentially several different points.
@@ -110,17 +14,24 @@ class PeriodicElectron:
     lvec and dist will most likely be references to the parent object
     """
 
-    def __init__(self, epos, lattice_vectors, dist, wrap=None):
+    def __init__(self, epos, kpt, lattice_vectors, dist, wrap=None):
         self.configs = epos
-        self.lvec = lattice_vectors
+        self.kpt = kpt
         self.wrap = wrap if wrap is not None else np.zeros_like(epos)
+        self.lvec = lattice_vectors
         self.dist = dist
 
 
-class PeriodicConfigs:
-    def __init__(self, configs, lattice_vectors, wrap=None):
+class KptConfigs:
+    def __init__(self, configs, kpts, lattice_vectors, wrap=None):
+        """
+        kpts define the qmc simulation cell
+        lattice_vectors: are for the primitive cell
+        wrap: store to apply phase, since we aren't tracking position outside primitive cell
+        """
         configs, wrap_ = pbc.enforce_pbc(lattice_vectors, configs)
         self.configs = configs
+        self.kpts = kpts
         self.wrap = wrap_
         if wrap is not None:
             self.wrap += wrap
@@ -128,17 +39,17 @@ class PeriodicConfigs:
         self.dist = distance.MinimalImageDistance(lattice_vectors)
 
     def electron(self, e):
-        return PeriodicElectron(
-            self.configs[:, e], self.lvecs, self.dist, wrap=self.wrap[:, e]
+        return KptElectron(
+            self.configs[:, e], self.kpts[:, e], self.lvecs, self.dist, wrap=self.wrap[:, e]
         )
 
     def mask(self, mask):
-        return PeriodicConfigs(self.configs[mask], self.lvecs, wrap=self.wrap[mask])
+        return KptConfigs(self.configs[mask], self.kpts[mask], self.lvecs, wrap=self.wrap[mask])
 
     def make_irreducible(self, e, vec, mask=None):
         """
         Input: a (nconfig, 3) vector or a (nconfig, N, 3) vector
-        Output: A Periodic Electron
+        Output: A Kpt Electron
         """
         if mask is None:
             mask = np.ones(vec.shape[0:-1], dtype=bool)
@@ -149,28 +60,30 @@ class PeriodicConfigs:
         if len(vec.shape) == 3:
             wrap = np.repeat(self.wrap[:, e][:, np.newaxis], vec.shape[1], axis=1)
         wrap[mask] += wrap_
-        return PeriodicElectron(epos, self.lvecs, wrap=wrap, dist=self.dist)
+        return KptElectron(epos, self.kpts[:, e], self.lvecs, wrap=wrap, dist=self.dist)
 
     def move(self, e, new, accept):
         """
         Change coordinates of one electron
         Args:
           e: int, electron index
-          new: PeriodicConfigs with (nconfig, 3) new coordinates
+          new: KptConfigs with (nconfig, 3) new coordinates
           accept: (nconfig,) boolean for which configs to update
         """
         self.configs[accept, e, :] = new.configs[accept, :]
         self.wrap[accept, e, :] = new.wrap[accept, :]
+        self.kpts[accept, e, :] = new.kpts[accept, :]
 
     def move_all(self, new, accept):
         """
         Change coordinates of all electrons
         Args:
-          new: PeriodicConfigs with configs.shape new coordinates
+          new: KptConfigs with configs.shape new coordinates
           accept: (nconfig,) boolean for which configs to update
         """
         self.configs[accept] = new.configs[accept]
         self.wrap[accept] = new.wrap[accept]
+        self.kpts[accept] = new.kpts[accept]
 
     def resample(self, newinds):
         """
@@ -180,6 +93,7 @@ class PeriodicConfigs:
         """
         self.configs = self.configs[newinds]
         self.wrap = self.wrap[newinds]
+        self.kpts = self.kpts[newinds]
 
     def split(self, npartitions):
         """
@@ -191,16 +105,18 @@ class PeriodicConfigs:
         """
         clist = np.array_split(self.configs, npartitions)
         wlist = np.array_split(self.wrap, npartitions)
-        return [PeriodicConfigs(c, self.lvecs, w) for c, w in zip(clist, wlist)]
+        klist = np.array_split(self.kpts, npartitions)
+        return [KptConfigs(c, k, self.lvecs, w) for c, w in zip(clist, klist, wlist)]
 
     def join(self, configslist, axis=0):
         """
         Merge configs into this object to collect from parallelization
         Args:
-          configslist: list of PeriodicConfigs objects
+          configslist: list of KptConfigs objects
         """
         self.configs = np.concatenate([c.configs for c in configslist], axis=axis)
         self.wrap = np.concatenate([c.wrap for c in configslist], axis=axis)
+        self.kpts = np.concatenate([c.kpts for c in configslist], axis=axis)
 
     def copy(self):
         return copy.deepcopy(self)
@@ -208,6 +124,7 @@ class PeriodicConfigs:
     def reshape(self, shape):
         self.configs = self.configs.reshape(shape)
         self.wrap = self.wrap.reshape(shape)
+        self.kpts = self.kpts.reshape(shape)
 
     def initialize_hdf(self, hdf):
         hdf.create_dataset(
@@ -219,15 +136,20 @@ class PeriodicConfigs:
         hdf.create_dataset(
             "wrap", self.wrap.shape, chunks=True, maxshape=(None, *self.wrap.shape[1:])
         )
+        hdf.create_dataset(
+            "kpts", self.kpts.shape, chunks=True, maxshape=(None, *self.kpts.shape[1:])
+        )
 
     def to_hdf(self, hdf):
         hdf["configs"].resize(self.configs.shape)
         hdf["configs"].resize(self.wrap.shape)
         hdf["configs"][...] = self.configs
         hdf["wrap"][...] = self.wrap
+        hdf["kpts"][...] = self.kpts
 
     def load_hdf(self, hdf):
         # The ... seems to be necessary to avoid changing the dtype and screwing up
         # pyscf's calls.
         self.configs[...] = hdf["configs"][()]
         self.wrap[...] = hdf["wrap"][()]
+        self.kpts[...] = hdf["kpts"][()]
